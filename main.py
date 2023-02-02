5f4d95c4c632b2504a07cfbc8a32a466889b437e
arjun2924 <sohamthakur2924@gmail.com>
2023-02-02 15:12:57 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import random
+
+print("Welcome to Rock Paper and Scissors")
+startGame = input("Type y to play or n to exit: ")
+
+if startGame == 'n':
+    print("Untill next time goodbye!")
+else:
+    compArr = ['r', 'p', 's']
+    userInp = input("Enter rock(r), paper(p) or scissors(s): ")
+    compChoice = compArr[random.randint(0, 2)]
+    if userInp == 'r':
+        if compChoice == 'p':
+            print("You lose!")
+        elif compChoice == 'r':
+            print("Draw")
+        else:
+            print("You win!")
+    elif userInp == 'p':
+        if compChoice == 's':
+            print("You lose!")
+        elif compChoice == 'p':
+            print("Draw")
+        else:
+            print("You win!")
+    else:
+        if userInp == 's':
+            if compChoice == 'r':
+                print("You lose!")
+            elif compChoice == 's':
+                print("Draw")
+            else:
+                print("You win!")
